c145316f767c3b8f755afcb5e0fd06c3ba594927
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2022-01-10 22:09:45 -0700

    Test: logging in as a current user

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -11,9 +11,6 @@ RSpec.describe Merchant, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:username) }
-    it { should validate_uniqueness_of(:username) }
-    it { should validate_presence_of(:password) }
   end
 
   describe 'class methods' do
