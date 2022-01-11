1b9a2ad3da7c426bdfe8bfba7dd99eb0250e64fa
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2022-01-10 20:53:35 -0700

    Test: username and password model validations

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -11,6 +11,9 @@ RSpec.describe Merchant, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_presence_of(:password) }
   end
 
   describe 'class methods' do
