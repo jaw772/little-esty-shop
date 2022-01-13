18bb3cef748d9ab888a43746cbba72389054fea2
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2022-01-13 13:37:34 -0700

    Comment out api calling methods

--- app/poros/pulls.rb
+++ app/poros/pulls.rb
@@ -1,11 +1,11 @@
-class Pulls
-  attr_reader :number
-
-  def initialize(data)
-    if data.class == Array
-       @number = data[1]
-    else
-       @number = data[:number]
-    end
-  end
-end
+# class Pulls
+#   attr_reader :number
+#
+#   def initialize(data)
+#     if data.class == Array
+#        @number = data[1]
+#     else
+#        @number = data[:number]
+#     end
+#   end
+# end
