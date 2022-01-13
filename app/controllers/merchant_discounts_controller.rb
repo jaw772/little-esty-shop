e77f4aa981b056d86d5024414d662c2145606d63
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2022-01-13 16:23:01 -0700

    Feat: bulk discount create page

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -10,17 +10,19 @@ class MerchantDiscountsController < ApplicationController
   end
 
   def new
+    @merchant = Merchant.find(params[:merchant_id])
     @discount = Discount.create()
   end
 
   def create
-    @merchant = Merchant.find(params[:merchant_id])
-    @discount = Discount.create(discount_params)
+    merchant = Merchant.find(params[:merchant_id])
+    discount = merchant.discounts.create(discount_params)
+    redirect_to "/merchants/#{merchant.id}/discounts"
   end
 
   private
 
   def discount_params
     params.require(:discount).permit(:name, :discount_rate, :threshold_quantity)
-  end 
+  end
 end
--- spec/features/merchants/discounts/new_spec.rb
+++ spec/features/merchants/discounts/new_spec.rb
@@ -7,10 +7,11 @@ RSpec.describe "Merchant Discount New page" do
     visit "/merchants/#{merchant.id}/discounts/new"
 
     expect(page).to have_content("Add a new discount")
-    fill_in ":name", with: "20 For 20"
-    fill_in ":discount_rate", with: 0.20
-    fill_in ":threshold_quantity", with: 20
+    fill_in "discount_name", with: "20 For 20"
+    fill_in "discount_discount_rate", with: 0.20
+    fill_in "discount_threshold_quantity", with: 20
     click_on "Create Discount"
-    expect(current_path).to eq("/merchants/#{merchant1.id}/discounts")
+    expect(current_path).to eq("/merchants/#{merchant.id}/discounts")
+    expect(page).to have_link("20 For 20")
   end
-end 
+end
