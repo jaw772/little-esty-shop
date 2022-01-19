6ee8adfc4ed03bc1dc6afb839e9486da8799b310
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2022-01-18 21:19:14 -0700

    Feat: upcoming holidays implementation

--- app/facades/header_facade.rb
+++ app/facades/header_facade.rb
@@ -1,15 +1,21 @@
-# class HeaderFacade
-#   def repo
-#     service.repo[:name]
-#   end
-#   def contributors
-#     service.contributors.map { |data| Contributors.new(data) }
-#   end
-#   def pulls
-#     service.pulls.map { |data| Pulls.new(data) }
-#   end
-#
-#   def service
-#     GithubService.new
-#   end
-# end
+class HeaderFacade
+  # def repo
+  #   service.repo[:name]
+  # end
+  # def contributors
+  #   service.contributors.map { |data| Contributors.new(data) }
+  # end
+  # def pulls
+  #   service.pulls.map { |data| Pulls.new(data) }
+  # end
+  #
+  # def service
+  #   GithubService.new
+  # end
+  def usa
+    service.usa_dates.map { |data| UsaDates.new(data) }
+  end
+  def service
+    HolidayService.new
+  end
+end
--- spec/features/merchants/discounts/index_spec.rb
+++ spec/features/merchants/discounts/index_spec.rb
@@ -25,10 +25,12 @@ RSpec.describe "Merchant Discounts Index page" do
   end
 
   it 'displays a section with a header of the next 3 upcoming holidays' do
-    visit "/merchants/#{merchant.id}/discounts/"
+    merchant = create(:merchant)
+    discount1 = create(:discount, merchant: merchant, discount_rate: 0.3, threshold_quantity: 30, name: "30 For 30")
 
-    expect(page).to have_content("Valentine's Day")
-    expect(page).to have_content("St. Patricks Day")
-    expect(page).to have_content("Easter")
-  end 
+    visit "/merchants/#{merchant.id}/discounts/"
+    expect(page).to have_content("Presidents Day")
+    expect(page).to have_content("Good Friday")
+    expect(page).to have_content("Memorial Day")
+  end
 end
